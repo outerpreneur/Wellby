ccce5c65acc835876aba19960d112d4b17538099
Aleksandery\ Tysklind <info@aleksandertysklind.com>
2022-02-24 11:13:26 +0100

    javascript fixed

--- assets/application.js
+++ assets/application.js
@@ -1,6 +1,7 @@
 // Put your applicaiton javascript here
 $(document).ready(function () {
-    $(document.on('click', 'js-quantity-button', function (event) {
+
+    $(document.on('click', '.js-quantity-button', function(event) {
         alert('button clicked');
     });
 });
