cd694c74d175e14a827b16b88725aaccea89cfd0
Aleksandery\ Tysklind <info@aleksandertysklind.com>
2022-02-24 12:34:30 +0100

    see if it works

--- assets/application.js
+++ assets/application.js
@@ -1,7 +1,12 @@
 // Put your applicaiton javascript here
-$(document).ready(function() {
+newFunction()
 
-    $(document.on('click', '.js-quantity-button', function(event) {
-     alert('button clicked');
-    });
-});
+function newFunction() {
+  $(document).ready(function () {
+    $(
+      document.on('click', '.js-quantity-button', function (event) {
+        alert('button clicked')
+      })
+    )
+  })
+}
