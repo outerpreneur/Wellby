467066643fd67594555d0a9b416ae5141f244a26
Aleksandery\ Tysklind <info@aleksandertysklind.com>
2022-02-24 11:10:03 +0100

    jquery added and javascript on button  product

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,6 @@
 // Put your applicaiton javascript here
+$(document).ready(function () {
+    $(document.on('click', 'js-quantity-button', function (event) {
+        alert('button clicked');
+    });
+});
